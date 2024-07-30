f68ecd138a3f2fb53346e3db572d899f0dd45908
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2024-07-30 19:49:41 +0530

    Init Commit

---
+++ script.js
@@ -0,0 +1,340 @@
+const questions = [
+    {
+        question: "Which of the following is a universal force that exist between two bodies due to their masses?",
+        optionA: "Weak Nuclear Force",
+        optionB: "Strong Nuclear Force",
+        optionC: "Gravitational Force",
+        optionD: "Electromagnetic Force",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following is not one of the four fundamental forces in nature?",
+        optionA: "Strong Nuclear",
+        optionB: "Frictional Force",
+        optionC: "Gravity",
+        optionD: "EM Force",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Who out of the given is known ad Father of Experimental Physics?",
+        optionA: "Rutherford",
+        optionB: "Sir Issav Newton",
+        optionC: "Faraday",
+        optionD: "Gallileo",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What force or forces exist between a proton and a second proton?",
+        optionA: "The weak electrostatic and strong magnetic",
+        optionB: "the weak electrostatic and string gravitational",
+        optionC: "The weak nuclear, strong nuclear and strong electromagnetic",
+        optionD: "The weak frictional and strong gravitational",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Size of the universe is of the order of ?",
+        optionA: "10^18 m",
+        optionB: "10^26 m",
+        optionC: "10^-40",
+        optionD: "10^-18",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "After the Big Bang, what was the first force to separate from the others ?",
+        optionA: "Electromagnetic Force",
+        optionB: "Gravity",
+        optionC: "Strong Nuclear Force",
+        optionD: "Weak Nuclear Force",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The gravitational force were discovered by ?",
+        optionA: "Gallileo",
+        optionB: "Kepler",
+        optionC: "Newton",
+        optionD: "Cavendish",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following best describes why knowledge of physics is necessary to understand all other sciences ?",
+        optionA: "Physics explains how energy passes from one object to another.",
+        optionB: "Physics explains how gravity works.",
+        optionC: "Physics explains the motion of objects that can be seen with the naked eye.",
+        optionD: "Physics explains the fundamental aspects of the universe.",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which of the following choices lists the four known types of forces in nature in order of decreasing strength ?",
+        optionA: "strong nuclear, electromagnetic, weak nuclear, gravitational",
+        optionB: "electromagnetic, strong nuclear, weak nuclear, gravitational",
+        optionC: "strong nuclear, gravitational, weak nuclear, electromagnetic",
+        optionD: "strong nuclear, weak nuclear, electromagnetic, gravitational",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "The magnitude of the gravitational force on the centre of the earth is",
+        optionA: "Maximum",
+        optionB: "Minimum",
+        optionC: "Zero",
+        optionD: "Unknown",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "The force due which the negatively charged electron revolve around the positively charged nucleus is... ?",
+        optionA: "Gravitational force",
+        optionB: "Electrostatic force",
+        optionC: "Nuclear force",
+        optionD: "Atomic force",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The Strongest Force among the following is......... ?",
+        optionA: "Gravitational force",
+        optionB: " Electrostatic force",
+        optionC: "Nuclear force",
+        optionD: "Electromagnetic force",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Range of Nuclear Force is..... ?",
+        optionA: "Smaller than Ten's Power Minus 14",
+        optionB: "Greater than Ten's Power Minus 14",
+        optionC: "Smaller than Ten's Power Minus 10",
+        optionD: "Greater than Ten's Minus 10",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which among the following is following is the weakest force ?",
+        optionA: "Gravitational force ",
+        optionB: "Electrostatic force",
+        optionC: "Nuclear force",
+        optionD: "Electromagnetic force",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Physics is considered as ?",
+        optionA: "Derived Science",
+        optionB: "Applied Science",
+        optionC: "Natural Science",
+        optionD: "Solution Science",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The final step of applying a scientific method is _________________ ?",
+        optionA: "Developing theory",
+        optionB: "Performing test",
+        optionC: "Analysis of test result",
+        optionD: "Formulation of a question",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Microscopic phenomenon’s are explained in ?",
+        optionA: "Faraday’s theory of electromagnetis",
+        optionB: "Quantum theory",
+        optionC: "Newton’s laws of motion",
+        optionD: "Fermi paradox",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Who discovered Neutron ?",
+        optionA: "Gallileo",
+        optionB: "Chadwick",
+        optionC: "C.V.Raman",
+        optionD: "None of them",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Theory of Relativity was discovered by ?",
+        optionA: "Maxwel",
+        optionB: "Faraday",
+        optionC: "Einstein",
+        optionD: "Pauli",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following Technology uses Newton's Second and Third Laws of Motion ?",
+        optionA: "Electric Generator",
+        optionB: "Radio and Television",
+        optionC: "Rocket Propulsion",
+        optionD: "None",
+        correctOption: "optionC"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() { 
+    //function to shuffle and push 20 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 20) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0
+
+// function for displaying next question in the array to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+   
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 20) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 6) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 8 && playerScore < 14) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 14) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 20) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
